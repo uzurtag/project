826bf429f4f0b5e97a90444fa8b5d001c94c4b04
Dmitriy Gulenko <ntlprodima@gmail.com>
2016-07-07 13:25:15 +0300

    css

--- frontend/views/site/detailProducts.php
+++ frontend/views/site/detailProducts.php
@@ -4,8 +4,8 @@
 
 <b>Price</b> <?=$products->price?>   $<br><br><br>
 
+<b>Description</b><br> <?=$products->description_ru?><br>
+
 <b>Date Create</b> <?=$products->date_create?><br><br><br>
 
 <b>Date Update</b> <?=$products->date_update?><br><br><br>
-
-<b>Description</b><br> <?=$products->description_ru?><br>
--- frontend/views/site/products.php
+++ frontend/views/site/products.php
@@ -1,10 +1,15 @@
-test products
+<!-- test products -->
 
 <h1>Products</h1>
 
+
+
 <?php foreach ($products as $item): ?>
-	<div class="pre-news">
+	<div class="product">
+		<img src="/frontend/web/images/xiaomi_mi5_1638652733.jpg" style="width: 200px;">
 		<a href="index.php?r=site%2Fdetail-products&id=<?=$item->id?>"><h2> <?=$item->title_ru?> </h2></a>
+		<span><?=$item->price?> $</span>
+		<button type="button" class="btn btn-success">В корзину</button>
 	</div>
 <?php endforeach; ?>
 
